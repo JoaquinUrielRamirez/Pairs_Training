310dd066d9a42f532ef3596be5f2d5f80ca006d3
Joaquín Uriel Ramirez Péerz <uriel.ram310@gmail.com>
2025-03-05 13:46:20 -0600

    L

--- correlacion.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def corr(corr_matrix):
-    np.fill_diagonal(corr_matrix.values, np.nan)  # Remplaza la diagonal con NAN
-    max_corr_value = corr_matrix.max().max()
-    max_corr_pair = corr_matrix.stack().idxmax()  # Encuentra las variables con mayor correlación
-
-    return max_corr_value, max_corr_pair
--- datos_yfinance.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-def data_(tickers, start):
-    o_general = yf.download(tickers, start=start)["Close"]
-    o_general = o_general.dropna()
-    tipo_de_cambio = o_general['MXN=X']
-    general = o_general
-    return o_general, tipo_de_cambio
